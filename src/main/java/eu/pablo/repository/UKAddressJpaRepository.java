8e1b62db636bf1fd66bdef9471902aed807d393e
Pablo López Martínez <pabloplm@gmail.com>
2017-01-28 13:17:20 +0100

    hotfix/imports

--- src/main/java/eu/pablo/repository/UKAddressJpaRepository.java
+++ src/main/java/eu/pablo/repository/UKAddressJpaRepository.java
@@ -5,7 +5,6 @@ import java.io.Serializable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import eu.pablo.entity.IrelandAddress;
 import eu.pablo.entity.UKAddress;
 
 @Repository("ukAddressJpaRepository")
